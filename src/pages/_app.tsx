97d7882cd96cbc0d32b9f60e524ac489b9498f11
ABDUL-KABUGU <kabuguabdul2@gmail.comL>
2022-03-19 16:10:39 +0300

    logo changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,7 +34,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <StyledEngineProvider injectFirst>
       <Head>
-        <title>rSocial</title>
+        <title>debble</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <I18nextProvider i18n={i18n}>
@@ -49,7 +49,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                     onSidebarClose={handleMobileBurgerClick}
                   />
                   <Header
-                    label="rSocial"
+                    label="debble"
                     onMobileBurgerClick={handleMobileBurgerClick}
                     isShowingMobileBurger={isShowingMobileBurger}
                   />
